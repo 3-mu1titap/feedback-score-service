a1fcddf9672f2ff3e2f621ce857feecf536782c9
kang-subin <tnbin98@gmail.com>
2024-12-05 19:39:05 +0900

    Feat: 피드백 요소 조회 api 구현

--- src/main/java/com/multitap/feedback/application/ScoreServiceImpl.java
+++ src/main/java/com/multitap/feedback/application/ScoreServiceImpl.java
@@ -1,22 +1,38 @@
 package com.multitap.feedback.application;
 
+import com.multitap.feedback.common.exception.BaseException;
+import com.multitap.feedback.common.response.BaseResponseStatus;
+import com.multitap.feedback.dto.FeedbackElementResponseDto;
 import com.multitap.feedback.dto.FeedbackScoreRequestDto;
+import com.multitap.feedback.entity.FeedbackElement;
+import com.multitap.feedback.infrastructure.FeedbackElementRepository;
 import com.multitap.feedback.infrastructure.FeedbackScoreRepository;
 import com.multitap.feedback.kafka.producer.FeedbackScoreDto;
 import com.multitap.feedback.kafka.producer.KafkaProducerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class ScoreServiceImpl implements ScoreService {
 
     private final FeedbackScoreRepository feedbackScoreRepository;
+    private final FeedbackElementRepository feedbackElementRepository;
     private final KafkaProducerService kafkaProducerService;
 
     @Override
-    //todo: 멘토 이름도 가져오도록 훈석님한테 물어보기
     public void addScore(FeedbackScoreRequestDto feedbackScoreRequestDto) {
         kafkaProducerService.sendFeedbackScore(FeedbackScoreDto.from(feedbackScoreRepository.save(feedbackScoreRequestDto.toEntity(feedbackScoreRequestDto))));
+    }
+
+    @Override
+    public List<FeedbackElementResponseDto> getFeedbackElement(String categoryCode) {
+       return feedbackElementRepository.findByCategoryCode(categoryCode)
+               .stream()
+               .map(FeedbackElementResponseDto::from)
+               .toList();
     }
 }
